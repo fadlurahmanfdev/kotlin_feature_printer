442da4b9e095a2416ef8c41c1eabdeb7d52bd042
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2025-01-27 13:45:17 +0700

    feat(ci-cd): kotlin feature printer

--- feature_printer/build.gradle.kts
+++ feature_printer/build.gradle.kts
@@ -1,6 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
+
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 android {
@@ -42,4 +47,37 @@ dependencies {
 //    androidTestImplementation(libs.androidx.espresso.core)
 
     api ("com.github.DantSu:ESCPOS-ThermalPrinter-Android:3.4.0")
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("com.fadlurahmanfdev", "kotlin_feature_printer", "0.0.1")
+
+    pom {
+        name.set("Kotlin Feature Printer")
+        description.set("Library for handle bluetooth printer")
+        inceptionYear.set("2025")
+        url.set("https://github.com/fadlurahmanfdev/kotlin_feature_printer/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("fadlurahmanfdev")
+                name.set("Taufik Fadlurahman Fajari")
+                url.set("https://github.com/fadlurahmanfdev/")
+            }
+        }
+        scm {
+            url.set("https://github.com/fadlurahmanfdev/kotlin_feature_printer/")
+            connection.set("scm:git:git://github.com/fadlurahmanfdev/kotlin_feature_printer.git")
+            developerConnection.set("scm:git:ssh://git@github.com/fadlurahmanfdev/kotlin_feature_printer.git")
+        }
+    }
 }
